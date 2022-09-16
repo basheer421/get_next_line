160e3fb6652900ff443b28396994427b8d9e5e36
basheer421 <basheer123b2@gmail.com>
2022-09-16 18:26:55 +0400

    headerfile fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,18 +6,15 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 18:43:52 by bammar            #+#    #+#             */
-/*   Updated: 2022/09/15 20:05:40 by bammar           ###   ########.fr       */
+/*   Updated: 2022/09/16 18:25:38 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-
-// # define BUFFER_SIZE 5
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
